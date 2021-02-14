6370f4b64ef0f5174e5a1364d2f917fcf3eb3cbc
User <User@DESKTOP-5IFU6NT>
2021-02-14 01:50:36 -0800

    front end completly loaded.

--- SpadCompanyPanel.Web/Content/client/js/map.js
+++ SpadCompanyPanel.Web/Content/client/js/map.js
@@ -1,68 +1,215 @@
- // pickup map
-  var locations = [
-    ['Sausalito', 37.8590937, -122.4852507, 'assets/img/delivery-location.png'],
-    ['Sacramento', 38.5815719, -121.4943996, 'assets/img/pick-location.jpg'],
-  ];
-  var map = new google.maps.Map(document.getElementById('pickupmap'), {
-    zoom: 6,
-    center: {
-      lat: 37.8590937,
-      lng: -122.4852507
+                    // When the window has finished loading create our google map below
+                    google.maps.event.addDomListener(window, 'load', init);
+
+                    function init() {
+                        // Basic options for a simple Google Map
+                        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+                        var myLatlng = new google.maps.LatLng(28.424653, -81.469516);
+
+                        var mapOptions = {
+                            // How zoomed in you want the map to start at (always required)
+                            zoom: 15,
+                            disableDefaultUI: true,
+							scrollwheel: false, 
+
+                            // The latitude and longitude to center the map (always required)
+
+                            center: myLatlng, // New York
+
+                            // How you would like to style the map. 
+                            // This is where you would paste any style found on Snazzy Maps.
+                            styles: [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
     },
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-  var infowindow = new google.maps.InfoWindow();
-  var marker, i;
-  for (i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-      map: map,
-      icon: locations[i][3]
-    });
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-      return function () {
-        infowindow.setContent(locations[i][0]);
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
-  }
-  var line = new google.maps.Polyline({
-    path: [new google.maps.LatLng(37.8590937, -122.4852507), new google.maps.LatLng(38.5815719, -121.4943996)],
-    strokeColor: "#00a000",
-    strokeOpacity: 1.0,
-    strokeWeight: 5,
-    geodesic: true,
-    map: map
-  });
-  
-  var map = new google.maps.Map(document.getElementById('pickupmap2'), {
-    zoom: 8,
-    center: {
-      lat: 37.8590937,
-      lng: -122.4852507
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
     },
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
-  var infowindow = new google.maps.InfoWindow();
-  var marker, i;
-  for (i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-      map: map,
-      icon: locations[i][3]
-    });
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-      return function () {
-        infowindow.setContent(locations[i][0]);
-        infowindow.open(map, marker);
-      }
-    })(marker, i));
-  }
-  var line = new google.maps.Polyline({
-    path: [new google.maps.LatLng(37.8590937, -122.4852507), new google.maps.LatLng(38.5815719, -121.4943996)],
-    strokeColor: "#00a000",
-    strokeOpacity: 1.0,
-    strokeWeight: 5,
-    geodesic: true,
-    map: map
-  });
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+]
+                        };
+
+                        // Get the HTML DOM element that will contain your map 
+                        // We are using a div with id="map" seen below in the <body>
+                        var mapElement = document.getElementById('map');
+
+                        // Create the Google Map using out element and options defined above
+                        var map = new google.maps.Map(mapElement, mapOptions);
+						
+                        var marker = new google.maps.Marker({
+                            position: myLatlng,
+                            map: map,
+							icon: 'images/map-marker.png',
+                            title: 'Lorem Ipsum'
+                        });
+						
+						
+
+                    }
